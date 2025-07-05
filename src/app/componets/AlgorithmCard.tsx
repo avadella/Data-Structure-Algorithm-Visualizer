68c7969d8f44aae04190419fefc0dc74818a9b06
Baker, Noah <noahchbaker@gmail.com>
2025-07-05 13:44:27 -0400

    Array Visualization - Shuffle Insert Reset

--- src/app/componets/AlgorithmCard.tsx
+++ src/app/componets/AlgorithmCard.tsx
@@ -1,21 +1,26 @@
+import { link } from "fs";
+import Link from "next/link";
 import React from 'react'
 
 interface AlgorithmCardData {
     key: number;
     title: string;
     description: string;
+    routerPath: string
     imagePath: string;
 }
 
 const AlgorithmCard = (props: AlgorithmCardData) => {
     return (
-        <div className = 'bg-softgray rounded-lg p-4'>
-            <h1 className = 'text-2xl  text-fontLight text-center mb-2'> {props.title} </h1>
-            <div className = 'grid grid-cols-2 bg-boxbg rounded-md overflow-hidden'>
-                <img src={props.imagePath} alt="test image" />
-                <h1 className = 'px-5 py-3 text-fontDark'> {props.description} </h1>
+        <Link href = {props.routerPath}>
+            <div className = 'bg-softgray rounded-lg p-4'>
+                <h1 className = 'text-2xl  text-fontLight text-center mb-2'> {props.title} </h1>
+                <div className = 'grid grid-cols-2 bg-boxbg rounded-md overflow-hidden'>
+                    <img src={props.imagePath} alt="test image" />
+                    <h1 className = 'px-5 py-3 text-fontDark'> {props.description} </h1>
+                </div>
             </div>
-        </div>
+        </Link>
     )
 }
 
